3757956c5b9f300eda2e225e863d6679aab3e479
cds183 <cherylds.mg11@nycu.edu.tw>
2024-10-17 22:37:53 +0800

    test

---
+++ backend/routes/user.js
@@ -0,0 +1,40 @@
+// 確認是不是公司員工(登入檢查)
+
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User'); 
+
+router.post('/check-user', async (req, res) => {
+    const { userId, password } = req.body;
+    
+    // 密碼格式化為 YYYY-MM-DD 格式
+    const formattedPassword = `${password.substring(0, 4)}-${password.substring(4, 6)}-${password.substring(6, 8)}`;
+    console.log(`userId: ${userId}, formattedPassword: ${formattedPassword}`);
+    
+    try {
+      // 查找用戶
+      const user = await User.findOne({ _id:userId, Date_Of_Birth:formattedPassword});
+      console.log(`${user}`)
+
+      if (user) {
+        console.log(`I Find you~`)
+        if (user.Status === 'Terminated') {
+          // 如果 Status 是 Terminated，回傳 exists: false
+          return res.json({ exists: false });
+        }
+        
+        // 存到session中
+        req.session.userId = userId;
+        console.log(`Session userId set: ${req.session.userId}`); 
+        
+        return res.json({ exists: true });
+      } else {
+        return res.json({ exists: false });
+      }
+    } catch (error) {
+      console.error('Error querying MongoDB:', error);
+      res.status(500).json({ error: 'Server error' });
+    }
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const session= require('express-session');
+require('dotenv').config();  //加載.env檔裡面的環境變量
+const mongoURI = process.env.MONGODB_URI;
+const sessionSECRET = process.env.SESSION_SECRET;
+
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',  
+  credentials: true 
+}));
+app.use(express.json());
+
+app.use(session({
+  secret: sessionSECRET,  
+  resave: false,              
+  saveUninitialized: false,   
+  cookie: { 
+    secure: false,
+    httpOnly: true,          
+    sameSite: 'lax'  }   
+}));
+
+// 連結MongoDB 
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+const userRoutes = require('./routes/user');
+const authRoutes = require('./routes/auth');
+const pictureRoutes = require('./routes/picture');
+const nickRoutes = require('./routes/nick');
+// const dashRoutes = require('./routes/dashboard');
+// const expRoutes = require('./routes/experience');
+
+// 使用路由
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/picture', pictureRoutes);
+app.use('/api/nickname', nickRoutes);
+// app.use('/api/dashboard', dashRoutes);
+// app.use('/api/experience', expRoutes);
+
+// 啟動服務器
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
